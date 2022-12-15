e5ce66775e8d8b9643ed2f6d65f21f5dbaa8bb42
dtwostar <kencs16358@gmail.com>
2022-12-15 15:51:17 +0800

    version1
    Signed-off-by: dtwostar <kencs16358@gmail.com>

--- note.js
+++ note.js
@@ -1,30 +1,33 @@
 const d = new Date();
 const ts = d.valueOf();
+//x++ ++x的使用
 let x = 2;
 let y = 2;
 const r1 = x++ + x++;
+//延後遞增
+//(2+(x++))
+//(2+3)
+//x = 4
 const r2 = ++x + ++x;
-const r3 = x++ + ++x;
-const r4 = ++x + x++;
-const r5 = y++;
+//預先遞增
+//x = 11
 console.log(r1)
 console.log(r2)
-console.log(r3)
-console.log(r4)
-console.log(r5)
 console.log(typeof (x))
-//x++ 先後計算
+
+// \的encode
 const roomTempC = 21.5;
 let currntTempC = 19.5;
 const message = `currntTempC is ` + `${currntTempC - roomTempC}\u00b0C`;
 console.log(message)
-// \的encode
 
+//var是全域變數，let是區域變數
 let hatPrice = 100;
 console.log(`Hat price: ${hatPrice}`);
 let bootsPrice = "100";
 console.log(`Boots price: ${bootsPrice}`);
 
+//javascript 功能變數指定法和簡寫法
 function sumPrices(...numbers) {
     return numbers.reduce(
         function (total, val) {
@@ -56,16 +59,15 @@ console.log(`Total: ${totalPrice}, ${typeof totalPrice}`);
 totalPrice = sumPrices(100, 200, undefined, false, "hello");
 console.log(`Total: ${totalPrice}, ${typeof totalPrice}`);*/
 totalPrice = sumPrices(100, 200, 300);
-//console.log(`Total: ${totalPrice}, ${typeof totalPrice}`);
-//javascript 功能變數指定法和簡寫法
 
 
+// 無限自我呼叫
 const g = function f(stop) {
     if (stop) console.log("f stopped");
     f(true);
 };
 
-// 無限自我呼叫
+//call的第一個引數就是this所綁定的值 this = bruce
 const bruce = { name: 'Ken' }
 function update(birthYear, occupation) {
     this.birthYear = birthYear;
@@ -73,40 +75,73 @@ function update(birthYear, occupation) {
 }
 update.call(bruce, 1998, 'enginner')
 console.log(bruce)
-// this 使用 bruce新增
 
+// var 全域定義函數
 var block = 5;
 if (block === 5) {
     var block = 4
     console.log(block)
 }
 console.log(block)
-// var 全域定義函數
+
+// fill宣告陣列全部都填充數字1
 const CopyArrElement = new Array(5).fill(1)
 console.log(CopyArrElement)
-// fill宣告陣列全部都填充數字1
+// 回傳陣列位置
 const arr = [{ id: 1, name: "kendy" }, { id: 2, name: "tom" }]
 console.log(arr.findIndex(o => o.id === 1))
-// 回傳陣列位置
-console.log(arr.find(o => o.id === 1))
 // 回傳值
+console.log(arr.find(o => o.id === 1))
+//取出轉換值
 const id = arr.map(x => x.id);
 console.log(id)
-//取出轉換值
+
 const team = ["t1", "rng"]
 const win = [3, 2]
 const fin = team.map((x, i) => ({ name: x, win: win[i] }))
 console.log(fin[0])
-//兩陣列結合
-const arrFindSqrt = [1, 17, 16, 5, 4, 16, 10, 49];
+//陣列前後增減函式
+const arrtest = [1, 2, 3]
+arrtest.push(4)
+console.log(arrtest)
+arrtest.pop()
+console.log(arrtest)
+arrtest.unshift(0)
+console.log(arrtest)
+arrtest.shift()
+console.log(arrtest)
+//陣列反轉和排列
+arrtest.reverse()
+console.log(arrtest)
+arrtest.sort()
+console.log(arrtest)
+// x是元數本身 i是索引開始尋找的位置
+const arrFindSqrt = [1, 17, 16, 5, 10, 49];
 let arrFindSqrtE = arrFindSqrt.find((x, i) => i > 2 && Number.isInteger(Math.sqrt(x)));
 console.log(arrFindSqrtE)
-// x是元數本身 i是索引起始位置
-
+//陣列數字相加
 const ReduceArr = [5, 7, 2, 4]
 const sumE = ReduceArr.reduce((a, x) => a += x, 0)
 console.log(sumE)
-//陣列數字相加
+
+//map和filter函式使用
+//1、map方法返回的新数组是原数组的映射，何为映射？就是和原数组的长度相同，数值做相应处理。
+//2、filter方法返回的值是过滤原数组后的新数组，和原数组长度不同，数值不变。
+const cart = [{ name: "Kem", prive: "75" }, { name: "Kefsfem", prive: "755" }]
+const namest = cart.map(x => x.name)
+console.log(namest)
+
+let array = [1, 2, 4, 6]
+let newArray = array.filter(item => item > 2)
+console.log(newArray)
+
+//reduce 使用 acc為初始值 
+
+const arrre = [1, 2, 3]
+const sum = arrre.reduce((acc, x) => acc + x, 0)
+console.log(sum)
+
+//MAP取出使用
 const userRole = new Map([
     ['u1', 'user1'],
     ['u2', 'user2'],
@@ -126,7 +161,8 @@ for (let r of userRole.values()) {
 for (let [u, r] of userRole.entries()) {
     console.log(`${u}:${r}`);
 }
-//MAP取出使用
+
+// Set陣列的值不會重複
 const roles = new Set()
 roles.add('User')
 roles.add('User')
@@ -135,13 +171,8 @@ for (let u of roles.keys()) {
     console.log(u);
 }
 console.log(roles.size);
-// Set陣列的值不會重複
 
-let hat = {
-    name: "Hat",
-    price: 100,
-};
-
+//json(javascript object notation)strigify 格式的字串呼叫
 let otherHat = { ...hat };
 console.log(`OtherHat: ${otherHat.name}, ${otherHat.price}`);
 
@@ -151,7 +182,7 @@ let replacedProperties = { ...hat, price: 10 };
 console.log(`Replaced: ${JSON.stringify(replacedProperties)}`);
 let { price, ...someProperties } = hat;
 console.log(`Selected: ${JSON.stringify(someProperties)}`);
-//json(javascript object notation)strigify 格式的字串呼叫
+
 
 function writeMessage(message) {
     console.log(`${this.greeting}, ${message}`);
@@ -162,7 +193,13 @@ let testmessage = inp => console.log(`${this.greeting}, ${inp}`);
 writeMessage("It is sunny today");
 testmessage("It is rain today")
 
-//沒用 let,const,var等關鍵字設定，this 在function中是全域物件
+//產生器
+function* rainbow() {
+    yield 'res';
+    yield 'rds';
+    yield 'ressdf';
+}
 
-//測試上傳
-
+for (let color of rainbow()) {
+    console.log(color);
+}
